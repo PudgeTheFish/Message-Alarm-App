373641b11362da50da2bb02256ba5f0db310bcd5
PudgeTheFish <samihahislam@gmail.com>
2016-11-27 21:59:26 -0600

    before editing TPFragment so that TimeSetListener is implemented in Main

---
+++ app/src/main/java/pudgethefish/messagealarmclock/AlarmReceiver.java
@@ -0,0 +1,8 @@
+package pudgethefish.messagealarmclock;
+
+/**
+ * Created by samihah on 11/27/16.
+ */
+
+public class AlarmReceiver {
+}
--- app/src/main/java/pudgethefish/messagealarmclock/MainActivity.java
+++ app/src/main/java/pudgethefish/messagealarmclock/MainActivity.java
@@ -1,5 +1,6 @@
 package pudgethefish.messagealarmclock;
 
+import android.app.AlarmManager;
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.app.TimePickerDialog;
@@ -9,13 +10,16 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.format.DateFormat;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 import android.widget.TimePicker;
-
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
     //to make our alarm manager
+    AlarmManager alarm_manager;
+    Button alarm_butt;
     Context context;
 
 
@@ -24,6 +28,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         this.context = this;
+
+        alarm_manager = (AlarmManager) getSystemService(ALARM_SERVICE);
+
+        //turn the alarm on
+        alarm_butt = (Button) findViewById(R.id.time_button);
+
+        alarm_butt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
+        //turn the alarm off
+        Button off_butt = (Button) findViewById(R.id.cancel_button);
+
+        off_butt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //TODO: change color of the buttons and cancel the alarm
+            }
+        });
+
 
     }
 
@@ -34,7 +62,6 @@ public class MainActivity extends AppCompatActivity {
 
     public static class TimePickerFragment extends DialogFragment
             implements TimePickerDialog.OnTimeSetListener {
-        private Context mcontext;
 
         @Override
         public Dialog onCreateDialog(Bundle savedInstanceState) {
@@ -50,7 +77,18 @@ public class MainActivity extends AppCompatActivity {
 
         public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
             // Do something with the time chosen by the user
+            String hour_string = String.valueOf(hourOfDay);
+            String minute_string = String.valueOf(minute);
+
+            if (hourOfDay > 12){
+                hour_string = String.valueOf(hourOfDay - 12);
+            }
+            if (minute < 10){
+                minute_string = "0" + minute_string;
+            }
         }
     }
+
+
 }
 
