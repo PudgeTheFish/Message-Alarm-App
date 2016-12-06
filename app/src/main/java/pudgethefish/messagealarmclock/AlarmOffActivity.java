a6b87669774fca49c7d1a22ab373774964342fcd
PudgeTheFish <samihahislam@gmail.com>
2016-12-06 10:28:46 -0600

    before refactoring AlarmOff w fragment

--- app/src/main/java/pudgethefish/messagealarmclock/AlarmOffActivity.java
+++ app/src/main/java/pudgethefish/messagealarmclock/AlarmOffActivity.java
@@ -3,17 +3,10 @@ package pudgethefish.messagealarmclock;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.app.Activity;
-import android.app.LoaderManager.LoaderCallbacks;
 
-import android.content.CursorLoader;
-import android.content.Loader;
-import android.database.Cursor;
-import android.net.Uri;
 import android.os.AsyncTask;
 
 import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.text.TextUtils;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
@@ -25,16 +18,6 @@ import android.widget.TextView;
 
 
 public class AlarmOffActivity extends Activity  {
-
-    /**
-     * Id to identity READ_CONTACTS permission request.
-     */
-    private static final int REQUEST_READ_CONTACTS = 0;
-
-    /**
-     * Keep track of the login task to ensure we can cancel it if requested.
-     */
-    private UserLoginTask mAuthTask = null;
 
     // UI references.
     private EditText mInputView;
@@ -57,8 +40,8 @@ public class AlarmOffActivity extends Activity  {
             }
         });
 
-        Button mEmailSignInButton = (Button) findViewById(R.id.submit_button);
-        mEmailSignInButton.setOnClickListener(new OnClickListener() {
+        Button button_submit = (Button) findViewById(R.id.submit_button);
+        button_submit.setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View view) {
                 attemptLogin();
@@ -66,87 +49,30 @@ public class AlarmOffActivity extends Activity  {
         });
     }
 
-
-    /**
-     * Callback received when a permissions request has been completed.
-     */
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
-                                           @NonNull int[] grantResults) {
-        if (grantResults.length == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-            //DO SOMETHING WHEN THE MESSAGE WAS ENTERED CORRECTLY
-            Log.wtf("MESSAGE ENTERED", "message was correct");
-        }
-
-    }
-
-
     /**
      * Attempts to sign in or register the account specified by the login form.
      * If there are form errors (invalid email, missing fields, etc.), the
      * errors are presented and no actual login attempt is made.
      */
     private void attemptLogin() {
-        if (mAuthTask != null) {
-            return;
-        }
-
         // Reset errors.
         mInputView.setError(null);
 
         // Store values at the time of the login attempt.
         String input = mInputView.getText().toString();
 
-        View focusView = null;
+        String testStr = "testing";
+        // return true if the input is correct
+        if (testStr.equals(input)){
+            Log.e("MESSAGE ENTERED", "the strings are equal***********************");
 
-        // perform the user login attempt.
-        mAuthTask = new UserLoginTask(input);
-        mAuthTask.execute((Void) null);
-
-    }
-
-
-
-    /**
-     * Represents an asynchronous login/registration task used to authenticate
-     * the user.
-     */
-    public class UserLoginTask extends AsyncTask<Void, Void, Boolean> {
-
-        private final String mInput;
-
-        UserLoginTask(String input) {
-            mInput = input;
+            finish();
+        }
+        else {
+            mInputView.setError(getString(R.string.error_incorrect_input));
+            mInputView.requestFocus();
         }
 
-        @Override
-        protected Boolean doInBackground(Void... params) {
-
-            String testStr = "testing";
-            // return true if the input is correct
-            if (testStr.equals(mInput)){
-                Log.e("MESSAGE ENTERED", "the strings are equal");
-            }
-            return testStr.equals(mInput);
-
-        }
-
-        @Override
-        protected void onPostExecute(final Boolean success) {
-            mAuthTask = null;
-
-            if (success) {
-                finish();
-            } else {
-                mInputView.setError(getString(R.string.error_incorrect_password));
-                mInputView.requestFocus();
-            }
-        }
-
-        @Override
-        protected void onCancelled() {
-            mAuthTask = null;
-        }
     }
 }
 
