0e72d4ce9a99684d5dd15bb7daef9754cc8b6690
PudgeTheFish <samihahislam@gmail.com>
2016-12-07 02:01:08 -0600

    finished the message functionality

--- app/src/main/java/pudgethefish/messagealarmclock/AlarmOffActivity.java
+++ app/src/main/java/pudgethefish/messagealarmclock/AlarmOffActivity.java
@@ -1,11 +1,15 @@
 package pudgethefish.messagealarmclock;
 
 import android.app.AlarmManager;
+//import android.app.Fragment;
+import android.support.v4.app.Fragment;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.KeyEvent;
@@ -19,91 +23,27 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 
-public class AlarmOffActivity extends Activity  {
+public class AlarmOffActivity extends FragmentActivity  {
 
-    private EditText mInputView;
-    private Context context;
-    MainActivity main = new MainActivity();
-
-    /*
-    public AlarmOffActivity(Context context){
-        this.context = context;
-    }
-    */
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alarm_off);
-        this.context = this;
-        // Set up the login form.
 
-        mInputView = (EditText) findViewById(R.id.input_message);
-        mInputView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+        if (savedInstanceState == null){
+            getSupportFragmentManager().beginTransaction().add(R.id.alarm_off_container,
+                    new AlarmOffFragment()).commit();
+        }
+        getSupportFragmentManager().addOnBackStackChangedListener(new FragmentManager.OnBackStackChangedListener() {
             @Override
-            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
-                if (id == R.id.login || id == EditorInfo.IME_NULL) {
-                    attemptLogin();
-                    return true;
+            public void onBackStackChanged() {
+                int backCount = getSupportFragmentManager().getBackStackEntryCount();
+                if (backCount == 0){
+                    finish();
                 }
-                return false;
             }
         });
-
-        Button button_submit = (Button) findViewById(R.id.submit_button);
-        button_submit.setOnClickListener(new OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                attemptLogin();
-            }
-        });
-    }
-
-
-    /**
-     * Attempts to turn off the alarm based on the user's input
-     * If the message entered is incorrect, the
-     * errors are presented and no additional action is taken.
-     */
-    public void attemptLogin() {
-        // Reset errors.
-        mInputView.setError(null);
-
-        // Store values at the time of the login attempt.
-        String input = mInputView.getText().toString();
-
-        String testStr = "testing";
-        // return true if the input is correct
-        if (testStr.equals(input)){
-            Log.e("MESSAGE ENTERED", "the strings are equal***********************");
-            //DO STUFF: turn alarm off, reset the pending intent
-            Intent myIntent = new Intent(this, AlarmReceiver.class);
-            PendingIntent pending_intent = PendingIntent
-                    .getBroadcast(AlarmOffActivity.this, 0, myIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-            AlarmManager alarmManager = MainActivity.getAlarmManager();
-            alarmManager.cancel(pending_intent);
-
-            //change the button color to indicate the alarm is turned off
-
-            Button alarm_button = main.getAlarm_button();
-            Button off_button = main.getOff_button();
-
-            //Context mContext = main.getContext();
-            //Button alarm_button = (Button) ((Activity)mContext).findViewById(R.id.time_button);
-            //Button off_button = (Button) ((Activity)mContext).findViewById(R.id.cancel_button);
-            int mColor = ContextCompat.getColor(this, R.color.colorCancelAlarm);
-            alarm_button.setBackgroundColor(mColor);
-            off_button.setBackgroundColor(mColor);
-
-            myIntent.putExtra("extra", false);
-            sendBroadcast(myIntent);
-
-            finish();
-        }
-        else {
-            mInputView.setError(getString(R.string.error_incorrect_input));
-            mInputView.requestFocus();
-        }
     }
 }
 
--- app/src/main/java/pudgethefish/messagealarmclock/AlarmOffFragment.java
+++ app/src/main/java/pudgethefish/messagealarmclock/AlarmOffFragment.java
@@ -1,12 +1,22 @@
 package pudgethefish.messagealarmclock;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 /**
@@ -14,37 +24,20 @@ import android.view.ViewGroup;
  * Activities that contain this fragment must implement the
  * {@link AlarmOffFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link AlarmOffFragment#newInstance} factory method to
+ * Use the {@link AlarmOffFragment} factory method to
  * create an instance of this fragment.
  */
 public class AlarmOffFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private EditText mInputView;
+    private Context context;
+    View mainView;
+    MainActivity main = new MainActivity();
 
     private OnFragmentInteractionListener mListener;
 
     public AlarmOffFragment() {
         // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment AlarmOffFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static AlarmOffFragment newInstance(String param1, String param2) {
-        AlarmOffFragment fragment = new AlarmOffFragment();
-        return fragment;
     }
 
     @Override
@@ -56,10 +49,34 @@ public class AlarmOffFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_alarm_off, container, false);
+
+        View rootView = inflater.inflate(R.layout.fragment_alarm_off, container, false);
+        mainView = inflater.inflate(R.layout.activity_main, container, false);
+
+        mInputView = (EditText) rootView.findViewById(R.id.input_message);
+        mInputView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
+                if (id == R.id.login || id == EditorInfo.IME_NULL) {
+                    attemptLogin();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        Button button_submit = (Button) rootView.findViewById(R.id.submit_button);
+        button_submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                attemptLogin();
+            }
+        });
+
+        return rootView;
     }
 
+    /*
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
@@ -71,11 +88,13 @@ public class AlarmOffFragment extends Fragment {
         }
     }
 
+
     @Override
     public void onDetach() {
         super.onDetach();
         mListener = null;
     }
+    */
 
     /**
      * This interface must be implemented by activities that contain this
@@ -90,5 +109,51 @@ public class AlarmOffFragment extends Fragment {
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction();
+    }
+
+    /**
+     * Attempts to turn off the alarm based on the user's input
+     * If the message entered is incorrect, the
+     * errors are presented and no additional action is taken.
+     */
+    public void attemptLogin() {
+        // Reset errors.
+        mInputView.setError(null);
+
+        // Store values at the time of the login attempt.
+        String input = mInputView.getText().toString();
+
+        String testStr = "testing";
+        // return true if the input is correct
+        if (testStr.equals(input)){
+            Log.e("MESSAGE ENTERED", "the strings are equal***********************");
+            //DO STUFF: turn alarm off, reset the pending intent
+            Intent myIntent = new Intent(getActivity(), AlarmReceiver.class);
+            PendingIntent pending_intent = PendingIntent
+                    .getBroadcast(getActivity(), 0, myIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+            AlarmManager alarmManager = MainActivity.getAlarmManager();
+            alarmManager.cancel(pending_intent);
+
+            //change the button color to indicate the alarm is turned off
+
+            //Button alarm_button = main.getAlarm_button();
+            //Button off_button = main.getOff_button();
+
+            Context mContext = main.getContext();
+            Button alarm_button = (Button) mainView.findViewById(R.id.time_button);
+            Button off_button = (Button) mainView.findViewById(R.id.cancel_button);
+            int mColor = ContextCompat.getColor(getActivity(), R.color.colorCancelAlarm);
+            alarm_button.setBackgroundColor(mColor);
+            off_button.setBackgroundColor(mColor);
+
+            myIntent.putExtra("extra", false);
+            getActivity().sendBroadcast(myIntent);
+
+            getActivity().finish();
+        }
+        else {
+            mInputView.setError(getString(R.string.error_incorrect_input));
+            mInputView.requestFocus();
+        }
     }
 }
